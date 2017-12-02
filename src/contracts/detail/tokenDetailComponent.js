4ecc1a4eac2dc82a318481cb4ea7c29365cf4e00
Matt Lovan <magrelo404@gmail.com>
2017-12-02 15:37:58 -0500

    indent

--- src/contracts/detail/tokenDetailComponent.js
+++ src/contracts/detail/tokenDetailComponent.js
@@ -160,96 +160,104 @@ class FormComponent extends Component {
 
         <div>
           <h3>Contract Details</h3>
-          <p>balance: {this.state.balance} ETH  ($ {this.round(this.state.exchangeRate * this.state.balance, 2)} USD)</p>
-          <p>tokens: {this.state.tokens}</p>
-          <p>contract owner: {this.state.owner}</p>
-          <p>owner can burn: {this.state.ownerCanBurn ? 'true': 'false'}</p>
-          <p>owner can drain: {this.state.ownerCanDrain ? 'true': 'false'}</p>
-          <p>base price: {this.state.tokenBasePrice} ETH  (${this.round(this.state.exchangeRate * this.state.tokenBasePrice, 2)} USD)</p>
-          <p>exponent: {this.state.tokenPriceExponent}</p>
-          <p>divisor: {this.state.tokenPriceExponentDivisor}</p>
-          <p>Network: {this.state.network}</p>
-          <p>Etherscan: &nbsp;
-            <a
-              href={"https://rinkeby.etherscan.io/address/" + this.state.contractInstanceAddress}
-              target="_blank">{this.state.contractInstanceAddress}
-            </a>
-          </p>
+          <div style={{marginLeft: '1em'}}>
+            <p>balance: {this.state.balance} ETH  ($ {this.round(this.state.exchangeRate * this.state.balance, 2)} USD)</p>
+            <p>tokens: {this.state.tokens}</p>
+            <p>contract owner: {this.state.owner}</p>
+            <p>owner can burn: {this.state.ownerCanBurn ? 'true': 'false'}</p>
+            <p>owner can drain: {this.state.ownerCanDrain ? 'true': 'false'}</p>
+            <p>base price: {this.state.tokenBasePrice} ETH  (${this.round(this.state.exchangeRate * this.state.tokenBasePrice, 2)} USD)</p>
+            <p>exponent: {this.state.tokenPriceExponent}</p>
+            <p>divisor: {this.state.tokenPriceExponentDivisor}</p>
+            <p>Network: {this.state.network}</p>
+            <p>Etherscan: &nbsp;
+              <a
+                href={"https://rinkeby.etherscan.io/address/" + this.state.contractInstanceAddress}
+                target="_blank">{this.state.contractInstanceAddress}
+              </a>
+            </p>
+          </div>
+
 
           <h3>Buy</h3>
-          <p>next buy price: {this.state.buyPrice} ETH  (${this.round(this.state.exchangeRate * this.state.buyPrice, 2)} USD)</p>
-          <button
-            style={{}}
-            className="pure-button pure-button-primary"
-            onClick={()=>{this.buyTokens()}}>Buy one token ($ {this.round(this.state.exchangeRate * this.state.buyPrice, 2)} USD)</button>
+          <div style={{marginLeft: '1em'}}>
+            <p>next buy price: {this.state.buyPrice} ETH  (${this.round(this.state.exchangeRate * this.state.buyPrice, 2)} USD)</p>
+            <button
+              style={{}}
+              className="pure-button pure-button-primary"
+              onClick={()=>{this.buyTokens()}}>Buy one token ($ {this.round(this.state.exchangeRate * this.state.buyPrice, 2)} USD)</button>
+          </div>
 
           <h3>Sell</h3>
-          <p>your token count: {this.state.activeAccountTokens}</p>
-          <p>next sell price: {this.state.sellPrice} ETH  (${this.round(this.state.exchangeRate * this.state.sellPrice, 2)} USD)</p>
-          <button
-            className="pure-button pure-button-primary"
-            onClick={()=>{this.sellTokens()}}>Sell one token (${this.round(this.state.exchangeRate * this.state.sellPrice, 2)} USD)</button>
+          <div style={{marginLeft: '1em'}}>
+            <p>your token count: {this.state.activeAccountTokens}</p>
+            <p>next sell price: {this.state.sellPrice} ETH  (${this.round(this.state.exchangeRate * this.state.sellPrice, 2)} USD)</p>
+            <button
+              className="pure-button pure-button-primary"
+              onClick={()=>{this.sellTokens()}}>Sell one token (${this.round(this.state.exchangeRate * this.state.sellPrice, 2)} USD)</button>
+          </div>
 
           {!this.state.activeAccountIsOwner ? null :
             <div>
               <h3>Owner Functions </h3>
+              <div style={{marginLeft: '1em'}}>
+                <form className="pure-form" onSubmit={this.burnTokens.bind(this)}>
 
-              <form className="pure-form" onSubmit={this.burnTokens.bind(this)}>
+                  <label>Burn Address</label>
+                  <input type="text" className="pure-input-1"
+                    type="text"
+                    value={this.state.burnAddress}
+                    onChange={(event)=>{this.setState({burnAddress: event.target.value})}}></input>
+                  <label>Burn Count</label>
+                  <input type="text" className="pure-input-1"
+                    type="number"
+                    value={this.state.burnCount}
+                    onChange={(event)=>{this.setState({burnCount: event.target.value})}}></input>
 
-                <label>Burn Address</label>
-                <input type="text" className="pure-input-1"
-                  type="text"
-                  value={this.state.burnAddress}
-                  onChange={(event)=>{this.setState({burnAddress: event.target.value})}}></input>
-                <label>Burn Count</label>
-                <input type="text" className="pure-input-1"
-                  type="number"
-                  value={this.state.burnCount}
-                  onChange={(event)=>{this.setState({burnCount: event.target.value})}}></input>
+                  <button className="pure-button pure-button-primary">Burn</button>
+                </form>
 
-                <button className="pure-button pure-button-primary">Burn</button>
-              </form>
+                <form className="pure-form" onSubmit={this.burnTokens.bind(this)}>
 
-              <form className="pure-form" onSubmit={this.burnTokens.bind(this)}>
+                  <label>Drain Amount</label>
+                  <input type="text" className="pure-input-1"
+                    type="number"
+                    value={this.state.drainAmount}
+                    onChange={(event)=>{this.setState({drainAmount: event.target.value})}}></input>
 
-                <label>Drain Amount</label>
-                <input type="text" className="pure-input-1"
-                  type="number"
-                  value={this.state.drainAmount}
-                  onChange={(event)=>{this.setState({drainAmount: event.target.value})}}></input>
-
-                <button className="pure-button pure-button-primary">Drain</button>
-              </form>
+                  <button className="pure-button pure-button-primary">Drain</button>
+                </form>
+              </div>
 
             </div>
           }
+
           <h3>Transactions ({this.state.history.length})</h3>
-
-          <table className="pure-table pure-table-horizontal">
-            <thead>
-              <tr>
-                <td>From</td>
-                <td>Value</td>
-                <td>Link</td>
-              </tr>
-            </thead>
-            <tbody>
-
-              {this.state.history.map((transaction)=>{
-                return <tr key={transaction.hash}>
-
-                    <td>{transaction.from}</td>
-                    <td>{transaction.value}</td>
-                    <td>
-                      <a target="_blank" href={"https://rinkeby.etherscan.io/tx/" + transaction.hash}> &rarr; </a>
-                    </td>
+          <div style={{marginLeft: '1em'}}>
+            <table className="pure-table pure-table-horizontal">
+              <thead>
+                <tr>
+                  <td>From</td>
+                  <td>Value</td>
+                  <td>Link</td>
                 </tr>
-              })}
+              </thead>
+              <tbody>
 
-            </tbody>
-          </table>
+                {this.state.history.map((transaction)=>{
+                  return <tr key={transaction.hash}>
 
+                      <td>{transaction.from}</td>
+                      <td>{transaction.value}</td>
+                      <td>
+                        <a target="_blank" href={"https://rinkeby.etherscan.io/tx/" + transaction.hash}> &rarr; </a>
+                      </td>
+                  </tr>
+                })}
 
+              </tbody>
+            </table>            
+          </div>
         </div>
         }
 
